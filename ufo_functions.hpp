15766ba54e4ff7baf491384fb0ba4ed4532ca0db
Luke Marques <74625391+Luke-Marques@users.noreply.github.com>
2021-08-18 18:38:08 +0100

    Create ufo_functions.hpp
    Added "ufo_functions.hpp" header file, which declares the functions contained in "ufo_functions.cpp" for use in the main function of "ufo.cpp".

---
+++ ufo_functions.hpp
@@ -0,0 +1,9 @@
+#include <vector>
+
+void display_misses(int misses);
+
+// Declare functions
+void greet();
+void end_game(std::string codeword, std::string answer);
+void display_status(std::vector<char>, std::string answer);
+char get_input();
